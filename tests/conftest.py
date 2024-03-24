80f1c1227cc716df25051391124f625c8ee29ef3
aban-m <aban.salah.mahmoud@gmail.com>
2024-03-24 20:49:04 +0200

    Added three simple tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,26 @@
 import pytest
 
 PICK = None
+PATIENCE = None
 
 def pytest_addoption(parser):
-    parser.addoption('--count', action='store', default='10')
+    parser.addoption('--count', action='store', default='3')
+    parser.addoption('--patience', action='store', default=20)
+    parser.addoption(
+        '--runslow', action='store_true', default=False,
+        help='Run simple network training routines.')
 
 def pytest_configure(config):
-    global PICK
+    global PICK, PATIENCE
     PICK = int(config.getoption('--count'))
+    PATIENCE = int(config.getoption('--patience'))
 
-def fun_with_flags(): return 'Indeed.'
+    config.addinivalue_line('markers', 'slow: mark a test as slow')
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption('--runslow'): return
+    skip_slow = pytest.mark.skip(reason='need --runslow to run')
+    for item in items:
+        if 'slow' in item.keywords:
+            item.add_marker(skip_slow)
+    
